994fd86f945980fd376314f9dfd411f128e3b9f3
imneocoder <imneocoder@gmail.com>
2021-10-04 23:39:20 +0530

    frogger script: moving the frog

--- frogger/app.js
+++ frogger/app.js
@@ -2,5 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('.grid div');
     const scoreDisplay = document.querySelector('span');
     const startBtn = document.querySelector('.start');
+    const width = 9;
+    
+    let currentIndex = 76;
+    let timerId
 
+    //render frog on starting block
+    squares[currentIndex].classList.add('frog')
+
+    // write a function that will move the frog
+    function moveFrog(e) {
+        squares [currentIndex].classList.remove('frog')
+        switch(e.keyCode) {
+            case 37:
+            if (currentIndex % width !== 0) currentIndex -=1
+            break;
+            
+        }
+    }
 })
