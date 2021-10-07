d3e85c264374dc1ff8af82334599be1d495fac54
imneocoder <imneocoder@gmail.com>
2021-10-07 08:08:05 +0530

    2048 grid formed

--- 2048/app.js
+++ 2048/app.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const griddisplay = document.querySelector('.grid')
-    const scoreDisplay = document.querySelector('.score')
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.getElementsById('score')
+    const resultDisplay = document.getElementById('result')
+    
+    //Create a Playing Board
+    function createBoard() {
+        for(leti=0; i <width*width; i++) {
+            square = document.createElement('div')
+            square.innerHTML = 0
+            gridDisplay.appendChild(square)
+            squares.push(square)
+        }
+    }
+    createBoard()
+
 })
