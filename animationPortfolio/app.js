e70a7728d16f405675ea0ad746e8a9df72922b9f
imneocoder <imneocoder@gmail.com>
2021-10-09 21:01:00 +0530

    animated Portfolio -deployment pending

--- animationPortfolio/app.js
+++ animationPortfolio/app.js
@@ -11,7 +11,24 @@ const balloonYFromBottom = 10
 
 function move() {
     const incrementer = window.scrollY
-    balloon.style.bottom = balloonYFromBottom + incrementer * 0.1 + '%'
+
+    // the '10' refers to the height from the bottom of the sky section . We added this  in the css file.
+    balloon.style.bottom = 10 + incrementer * 0.1 + '%'
+
+    cloudOne.style.bottom = 40 + incrementer * 0.12 + '%'
+    cloudOne.style.left = 70 + incrementer * 0.1 + '%'
+    cloudTwo.style.bottom = 80 + incrementer * 0.12 + '%'
+    cloudTwo.style.left = 70 + incrementer * 0.15 + '%'
+    cloudThree.style.bottom = 60 + incrementer * 0.14 + '%'
+    cloudThree.style.left = 0 + incrementer * 0.15 + '%'
+    cloudFour.style.bottom = 70 + incrementer * 0.14 + '%'
+    cloudFour.style.left = 20 + incrementer * 0.15 + '%'
+    cloudFive.style.bottom = 60 + incrementer * 0.2 + '%'
+    cloudFive.style.left = 60 + incrementer * 0.16 + '%'
+    javascript.style.left = 20 + incrementer * -2.2 + '%'
+    react.style.marginRight = 26 + incrementer * -1.5 + '%'
+    graphql.style.marginRight = 32 + incrementer * -1 + '%'
 }
 
 window.addEventListener('scroll', move)
+
